7e22f10f6f29dd54dd4c33f6620152b37d4b9ec9
Nahit-1 <nahit86@outlook.com>
2019-04-05 18:30:44 +0100

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+class Artist
+
+
+  attr_accessor :name
+
+  def self.name
+    @name = name
+  end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author
+
+ attr_accessor :name
+
+ def self.name
+   @name = name
+ end 
+
+
+
+
+
+end
